d3b47949ebccaa424f13a5de7ba6e9d9b5a2ab63
naveen <singhnaveen2105@gmail.com>
2022-10-31 01:57:10 +0530

    Initial commit

---
+++ src/main/java/neo/springdocker/Controller.java
@@ -0,0 +1,13 @@
+package neo.springdocker;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/wish")
+    public String wish(){
+        return "hell Neo";
+    }
+}
